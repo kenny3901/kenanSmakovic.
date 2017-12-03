fa79976611208c40026344c0aedd5c281642fc5e
kenny3901 <33628805+kenny3901@users.noreply.github.com>
2017-12-03 17:09:32 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+//The setup function only happens once
+function setup() {
+	createCanvas(500, 500); 
+}
+
+//The draw function happens over and over again
+function draw() {
+background(15,244,0);
+   stroke(0,220,255);
+   fill(0,50,255);
+    rect(125,125,250,250);
+
+
